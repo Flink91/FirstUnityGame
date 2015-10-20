c524e917f88eb90a9e79416d0296acadd923268a
Felix Gellner <flink@flinkblog.de>
2015-10-20 21:26:01 +0300

    First commit

---
+++ Assets/EasyMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EasyMove : MonoBehaviour {
+
+    public float speed = 4f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+         if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector2.up * speed * Time.deltaTime);
+        }
+    }
+}
